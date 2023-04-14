9d5b32dd9987511368802a9a80ea7eb97887928a
AdelRazzok <lhp6.arazzok@gmail.com>
2023-04-14 20:23:34 +0200

    ft_ft_isprint

--- libft.h
+++ libft.h
@@ -1,3 +1,5 @@
+#ifndef FT_LIBFT_H
+
 #include <stdio.h>
 
 int	ft_isalpha(int c);
@@ -6,3 +8,5 @@ int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
 size_t	ft_strlen(char *str);
+
+#endif
