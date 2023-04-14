877f570e9190301f46a5b04996d33c221cca05fc
AdelRazzok <lhp6.arazzok@gmail.com>
2023-04-14 20:18:00 +0200

    ft_strlen

--- libft.h
+++ libft.h
@@ -1,6 +1,8 @@
+#include <stdio.h>
+
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
-int	ft_strlen(char *str);
+size_t	ft_strlen(char *str);
--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "libft.h"
 
 int main(void)
