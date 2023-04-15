8512aafe86b011132fe02e78b1f633c36c53f4c2
AdelRazzok <lhp6.arazzok@gmail.com>
2023-04-15 04:10:27 +0200

    ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	if (!dest || !src)
+		return (0);
+	src_len = ft_strlen(src);
+	if (src_len <= 0)
+		return(src_len);
+	i = 0;
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
