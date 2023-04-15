a5dc9371d8d95fe5c57309e0121e02bcf8c9f226
AdelRazzok <lhp6.arazzok@gmail.com>
2023-04-15 02:33:20 +0200

    ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*dest_ptr;
 	const unsigned char	*src_ptr;
@@ -9,12 +9,16 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	dest_ptr = (unsigned char *)dest;
 	src_ptr = (const unsigned char *)src;
 	i = 0;
-	if (!dest_ptr || !src_ptr)
-		return (NULL);
-	while (i < n)
+	if (dest_ptr > src_ptr)
 	{
-		dest_ptr[i] = src_ptr[i];
-		i++;
+		while (n-- > 0)
+			dest_ptr[n] = src_ptr[n];
+	} else {
+		while (i < n)
+		{
+			dest_ptr[i] = src_ptr[i];
+			i++;
+		}
 	}
 	return (dest);
 }
